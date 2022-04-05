874ccd9618c0bfba01e59aafe2205be4581ebbee
Mwisya Julius <fm@gmail.com>
2022-04-05 14:58:29 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     return (
         <div className="App">
 
-            <Router >
+            <Router basename='/chawamihomes'>
                 
                 <Routes exact>
                     <Route path="/"  element={<Home/>}/>
