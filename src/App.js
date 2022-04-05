50e703a6111e02936a89ac26569094ef060a0c27
Mwisya Julius <fm@gmail.com>
2022-04-05 15:15:25 +0300

    deployment

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     return (
         <div className="App">
 
-            <Router basename='/chawamihomes'>
+            <Router >
                 
                 <Routes exact>
                     <Route path="/"  element={<Home/>}/>
