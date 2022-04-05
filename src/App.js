44a22ae57be1dfbc7080e4598e44d53b3e7482b9
Mwisya Julius <fm@gmail.com>
2022-04-05 14:02:26 +0300

    changes to browswer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css'
 import About from './pages/About/About';
 import Home from './pages/Home/Home';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+
 
 
 function App() {
     return (
         <div className="App">
-            <Routes>
-                <Route path="/"  element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-            </Routes>
+
+            <Router basename="/chawamihomes">
+                
+                <Routes>
+                    <Route path="/"  element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
+                </Routes>
+            </Router>
             
         </div>
     );
